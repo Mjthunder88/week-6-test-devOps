c2fe290db0e116f681344d38e51506f0a271c188
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2022-11-04 11:22:06 -0600

    Finished section 4

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -18,3 +18,20 @@ test('Title shows up when page loads', async () => {
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
 })
+
+test("shows if the choices for other robots display when the draw button is clicked", async () => {
+    await driver.findElement(By.id('draw')).click()
+    await driver.sleep(3000)
+     const displayed = await driver.findElement(By.id('choices')).isDisplayed()
+    expect(displayed).toBe(true)
+})
+
+test("Shows that if you click the add to duo button it will display the div with id player-duo",async () => {
+    await driver.findElement(By.id('draw')).click()
+    await driver.sleep(4000)
+    await driver.findElement(By.xpath('//button[contains(text(), "Add to Duo")]')).click()
+    await driver.sleep(3000)
+    let displayed = await driver.findElement(By.id('player-duo')).isDisplayed()    
+    expect(displayed).toBe(true)
+
+})
